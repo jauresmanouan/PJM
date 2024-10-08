c4b5cfecdb960b642ffe298cc92cfbd06620cd66
jauresmanouan <jauresmanouan@gmail.com>
2024-10-08 02:12:18 +0000

    📦 Add past index.css to install custom fonts and fix bugs after paste file

--- src/components/ui/pages/accueil/action/Action.tsx
+++ src/components/ui/pages/accueil/action/Action.tsx
@@ -9,22 +9,30 @@ export default function Action({
 }) {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="flex flex-col md:flex-row md:justify-center md:items-center md:gap-20 border-2 border-red-500">
-        <h1 className="flex flex-col justify-center items-center">
-          <div className="font-intro font-black text-7xl md:text-8xl text-jaune">
-            {number}
-          </div>
-          <div className="font-intro font-black text-5xl md:text-7xl text-bleu">
-            {action}
-          </div>
-        </h1>
-        <img
-          src="../../../../../../public/logoPJM2.svg"
-          alt="logo"
-          className="scale-[2] mt-3"
-        />
+      <img
+        src="../../../../../../public/logoPJM1.svg"
+        alt="logo"
+        className="w-40 h-full mb-4"
+      />
+      <div className="flex flex-col md:flex-row md:justify-center md:items-center md:gap-20">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="flex justify-center items-center">
+            <div className="font-intro font-black text-7xl md:text-7xl text-jaune">
+              {number}
+            </div>
+            <div className="font-intro font-black text-5xl md:text-6xl text-bleu">
+              {action}
+            </div>
+          </h1>
+          <p className="font-intro font-light  md:text-2xl text-bleu mb-4">
+            Pour la Joie de Marie
+          </p>
+        </div>
       </div>
-      <Button onClick={() => {}} className="border-2 border-blue-500 font-intro ">
+      <Button
+        onClick={() => {}}
+        className="font-intro w-60 transition duration-300 ease-in-out transform hover:scale-105"
+      >
         Participer
       </Button>
     </div>
