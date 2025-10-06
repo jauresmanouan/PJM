2ccd0529b9c10fec9e18dc944397ecdb4e403be2
jauresmanouan <jauresmanouan@gmail.com>
2025-10-06 11:40:24 +0000

    💄 Add background color white to Bilan, CallToAction, and HeaderBilan components for improved visual consistency

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -6,7 +6,7 @@ export default function CallToAction() {
   const { handleClicked } = useContext(Context);
 
   return (
-    <div className="h-60 sm:h-96 bg-fushia flex flex-col space-y-4 justify-center items-center text-center">
+    <div className="h-60 sm:h-96 bg-fushia flex flex-col space-y-4 justify-center items-center text-center bg-white">
       <p className="font-intro font-semibold text-lg md:text-3xl text-bleu px-5">
         Joignez votre rose aux nôtres et <br />
         offrons à Marie un beau bouquet 
